a8afac79e9a201cb72df7c0206d48f8089302647
Abhiram Nagishetti <abhiramnagishetti@Abhirams-MacBook-Air.local>
2026-07-17 16:36:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,193 @@
+alert()
+
+// Task1
+const companyName = "Stackly";
+let employeeName = prompt("Enter your name:");
+let employeeId = "EMP101";
+
+console.log("Company Name :", companyName);
+console.log("Employee Name :", employeeName);
+console.log("Employee ID :", employeeId);
+
+
+// Task2
+const collegeName = "St.Peter's Engineering College";
+let studentName = prompt("Enter your name");
+let courseName = prompt("Enter your course");
+
+console.log("College :", collegeName);
+console.log("Student :", studentName);
+console.log("Course :", courseName);
+
+
+// Task3
+const restaurentName = "Food High"
+let customerName = prompt("Enter your name");
+let foodName = prompt("Enter the food");
+let quantity = "2"
+
+console.log("Restaurent :", restaurentName);
+console.log("Customer :", customerName);
+console.log("Food :", foodName);
+console.log("Quantity :", quantity);
+
+
+// Task4
+const hospitalName = "KIMS";
+let patientName = prompt("Enter your name");
+let tokenNumber = "15";
+
+console.log("Hospital :", hospitalName);
+console.log("Patient :", patientName);
+console.log("Token :", tokenNumber);
+
+
+//Task5
+const theatreName = "Sandhya35mm";
+let movieName = prompt("Enter movie name");
+let viewerName = prompt("Enter your name");
+let seatNumber = "33";
+
+console.log("Theatre Name :", theatreName);
+console.log("Movie Name :", movieName);
+console.log("Customer Name :", viewerName);
+console.log("Seat Number :", seatNumber);
+
+
+// Task6
+const shopName = "AbhiMobiles";
+let customer1Name = prompt("Enter your name");
+let modelName = prompt("Enter your model");
+let price = "20,999/-";
+
+console.log("Shop Name :", shopName);
+console.log("Customer Name :", customer1Name);
+console.log("Model Name :", modelName);
+console.log("Price :", price);
+
+
+//Task7
+const gymName = "SKY GYM";
+let memberName = prompt("Enter your name");
+let membershipFee = "1599/-";
+
+console.log("Gym Name :", gymName);
+console.log("Member Name :", memberName);
+console.log("Membership Fee :", membershipFee);
+
+
+//Task8
+const bankName = "SBI";
+let userName = prompt("Enter your name");
+
+console.log("Bank :", bankName);
+console.log("User :", userName);
+
+
+//Task9
+const travelsName = "AbhiBus";
+let passengerName = prompt("Enter your name");
+let seat1Number = "35";
+
+console.log("Travels Name :", travelsName);
+console.log("Passenger Name :", passengerName);
+console.log("Seat Number :", seat1Number);
+
+
+//Task10
+const websiteName = "Myntra";
+let productNamme = prompt("Enter product name");
+let customer2Name = prompt("Enter your name");
+let pricee = "999/-"
+
+console.log("Website Name :", websiteName);
+console.log("Product Name :", productNamme);
+console.log("Customer Name :", customer2Name);
+console.log("price :", pricee);
+
+
+//Task11
+console.log("Registration Successful");
+
+
+//Task12
+console.error("Payment Failed");
+
+
+//Task13
+console.warn("Battery Low");
+
+
+//Task14
+console.log("Welcome");
+console.log("Loading...");
+console.clear();
+
+
+//Task15
+alert("Welcome to Amazon");
+
+
+//Task16
+let a = confirm("Do you want to logout?");
+console.log(a);
+
+
+//Task17
+let city = prompt("Enter your city");
+console.log("Your City :", city);
+
+
+//Task18
+var product1 = "Laptop";
+product1 = "Mobile";
+var product1 = "Tablet";
+console.log(product1);
+
+
+//Task19
+let salary = "25000";
+salary = "30000";
+console.log(salary);
+
+
+//Task20
+const companyy = "Google";
+console.log(companyy);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
